5fe8557dcd5323020514d1a92bd78e2d6a6fcf6e
YufanYang <yufan.yyf@alibaba-inc.com>
2019-02-15 17:01:47 +0800

    update

--- scripts/preview.js
+++ scripts/preview.js
@@ -2,9 +2,9 @@ const fs   = require("fs");
 const path = require("path");
 
 const rootDir = "preview/dist";
-const css     = fs.readFileSync(path.join(rootDir, "umi.css"), "utf-8");
+const css     = fs.readFileSync(path.join(rootDir, "index.css"), "utf-8");
 const html    = fs.readFileSync(path.join(rootDir, "index.html"), "utf-8");
-const js      = fs.readFileSync(path.join(rootDir, "umi.js"), "utf-8");
+const js      = fs.readFileSync(path.join(rootDir, "index.js"), "utf-8");
 
 const Data = { css, html, js };
 fs.writeFileSync("src/preview.json", JSON.stringify(Data));
