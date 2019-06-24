a2a2ad8af1e1c680f9e6faeca9609be8e17514ba
YufanYang <yufan.yyf@alibaba-inc.com>
2019-06-24 12:57:26 +0800

    update

--- src/library.js
+++ src/library.js
@@ -7,38 +7,47 @@ import fetch from 'sketch-polyfill-fetch';
 const Library = sketch.Library;
 
 const remoteLibrary = [
-  'O2kR2', // Anto 输出组件
-  'QxoYG', // Anto 视觉组件
-  'R9nlz', // Anto 交互组件
+  'anto-export', // Anto 输出组件
+  'anto-ui', // Anto 视觉组件
+  'anto-ux', // Anto 交互组件
 ];
 
 export const addLibrary = context => {
   _.forEach(remoteLibrary, fileName => {
-    const url = `https://client.sketch.cloud/v1/shares/${fileName}/rss`;
-
-    Library.getRemoteLibraryWithRSS(url, (err, lib) => {
-      if (err) return console.log(err);
-      fetch(url)
-        .then(response => response.text())
-        .then(text => {
-          const newVersion = text.match(/<pubDate>([^<]+?)<\/pubDate>/)[1];
-          const version = Settings.settingForKey(fileName);
-          const cacheLib = _.filter(
-            Library.getLibraries(),
-            lib => lib.name === 'Anto 输出组件' || lib.name === 'Anto 交互组件'
-          );
-          if (!version || version !== newVersion || cacheLib.length < 2) {
-            // remove old lib
-            lib.remove();
-            // add new one
-            Library.getRemoteLibraryWithRSS(url, (err, newlib) => {
-              if (err) return console.log(err);
-              Settings.setSettingForKey(fileName, newVersion);
-              UI.alert(newlib.name, '组件已经更新至：' + newVersion);
-            });
-          }
-        })
-        .catch(e => console.error(e));
+    const url = `http://anto.inc.alipay.net/static/library/${fileName}`;
+    UI.alert('title', url);
+    Library.getRemoteLibraryWithRSS(url, (err, library) => {
+      if (err) {
+        UI.alert('title', JSON.stringify(err));
+      }
+      if (library.name) {
+        UI.alert('title', library.name + ' Add Succeed!');
+        console.log('Conguratelation!');
+      }
     });
+    // Library.getRemoteLibraryWithRSS(url, (err, lib) => {
+    //   if (err) return console.log(err);
+    //   fetch(url)
+    //     .then(response => response.text())
+    //     .then(text => {
+    //       const newVersion = text.match(/<pubDate>([^<]+?)<\/pubDate>/)[1];
+    //       const version = Settings.settingForKey(fileName);
+    //       const cacheLib = _.filter(
+    //         Library.getLibraries(),
+    //         lib => lib.name === 'Anto 输出组件' || lib.name === 'Anto 交互组件'
+    //       );
+    //       if (!version || version !== newVersion || cacheLib.length < 2) {
+    //         // remove old lib
+    //         lib.remove();
+    //         // add new one
+    //         Library.getRemoteLibraryWithRSS(url, (err, newlib) => {
+    //           if (err) return console.log(err);
+    //           Settings.setSettingForKey(fileName, newVersion);
+    //           UI.alert(newlib.name, '组件已经更新至：' + newVersion);
+    //         });
+    //       }
+    //     })
+    //     .catch(e => console.error(e));
+    // });
   });
 };
